2860fedd5d3d7a4cba1598f45486bcfe4f8dc891
MdAsraful <mdasrafulislam7172@gmail.com>
2023-02-18 22:58:59 +0600

    first commite

--- js/work.js
+++ js/work.js
@@ -15,9 +15,11 @@ function calculation(firstCal,secondCal){
 
     const tr =document.createElement('tr') ;
     tr.innerHTML =`
-        <td>${1}</td>
+        <td>${'=>'}</td>
         <td>${firstCal}</td>
         <td> ${secondCal}</td>
+        
+        
 
     `;
     displayfieldOfOutpt.appendChild(tr);
@@ -29,10 +31,56 @@ document.getElementById('blog-btn').addEventListener('click', function(){
 })
 
 document.getElementById('Triangle-calculate-btn').addEventListener('click', function(){
-    const triangle=calculateTwoValue('Triangle-bed-field','Triangle-hight-field')   
-    const TriangelTotal=triangle*.5;
+    const triangle=calculateTwoValue('Triangle-bed-field','Triangle-hight-field');
+    const total=triangle*.5;   
+    const TriangelTotal=total.toFixed(2)
+    
 
-    calculation('Triangel',TriangelTotal)
-
+    calculation('Triangel',TriangelTotal);
     
 })
+
+document.getElementById('Rectangle-calculate-btn').addEventListener('click', function(){
+    const Rectangle=calculateTwoValue('Rectangle-width-field','Rectangle-hight-field');  
+    const RectangleTotal=Rectangle.toFixed(2);
+
+    calculation('Rectangle',RectangleTotal);
+    
+})
+
+document.getElementById('Parallelogram-calculate-btn').addEventListener('click', function(){
+    const Parallelogram=calculateTwoValue('Parallelogram-bed-field','Parallelogram-hight-field')   
+    const ParallelogramTotal=Parallelogram.toFixed(2);
+
+    calculation('Parallelogram',ParallelogramTotal);
+    
+})
+document.getElementById('Rhombus-calculate-btn').addEventListener('click', function(){
+    const Rhombus=calculateTwoValue('Rhombus-bed-field','Rhombus-hight-field');
+    const total=Rhombus*.5;   
+    const RhombusTotal=total.toFixed(2)
+    
+
+    calculation('Rhombus',RhombusTotal);
+    
+})
+document.getElementById('Pentagon-calculate-btn').addEventListener('click', function(){
+    const Pentagon=calculateTwoValue('Pentagon-bed-field','Pentagon-hight-field');
+    const total=Pentagon*.5;   
+    const PentagonTotal=total.toFixed(2)
+    
+
+    calculation('Pentagon',PentagonTotal);
+    
+})
+
+document.getElementById('Ellipse-calculate-btn').addEventListener('click', function(){
+    const Ellipse=calculateTwoValue('Ellipse-bed-field','Ellipse-hight-field');
+    const total=Ellipse*3.1416;   
+    const EllipseTotal=total.toFixed(2)
+    
+
+    calculation('Ellipse',EllipseTotal);
+    
+})
+
